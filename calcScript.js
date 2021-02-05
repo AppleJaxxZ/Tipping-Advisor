ee937d156f264c2d344a258d5a0b7dc65f7ed993
AppleJaxxZ <65720484+AppleJaxxZ@users.noreply.github.com>
2021-02-05 09:45:13 -0500

    All files added

---
+++ calcScript.js
@@ -0,0 +1,51 @@
+
+const calculate = document.getElementById("calculate");
+const returnContainer = document.getElementById('returnContainer');
+const inputField = document.getElementById('inputField');
+const clear = document.getElementById('remove');
+
+
+calculate.addEventListener('click', function() {
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+
+    if (document.getElementById("takeout").checked) {
+      paragraph.innerText = Math.round(inputField.value * .10) + '$';
+   } else if (document.getElementById("dineIn").checked) {
+      paragraph.innerText = Math.round(inputField.value * .15) + '$';
+   
+   } else if (document.getElementById('superiorService').checked) {
+      paragraph.innerText = Math.round(inputField.value * .20) + '$';
+   } 
+    
+ var checkRadio = document.querySelector( 
+    'input[name="percentage"]:checked'); 
+  
+if(checkRadio != null) { 
+   paragraph.innerText;
+} 
+else { 
+    paragraph.innerText
+        = "Please select a tip percentage."; 
+} 
+   
+      returnContainer.appendChild(paragraph);
+      inputField.value ="";
+      
+});
+
+clear.addEventListener('click', function() { 
+    
+  while (returnContainer.firstChild) {
+    returnContainer.removeChild(returnContainer.firstChild);
+    inputField.value = "";
+  }
+
+});
+
+inputField.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('calculate').click();
+    }
+})
